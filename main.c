58637fee82879112a9b4d4389620aee8d663515f
MartonioJunior <martoniojunior@hotmail.com>
2017-01-27 19:29:16 -0300

    Update main.c

--- main.c
+++ main.c
@@ -45,9 +45,33 @@ void searchMenu()
 }
 
 // Funções de busca
-cliente findClienteOnArray()
+cliente findClienteOnArray(char cpf[11], cliente[] clientes)
 {
-	
+	int index = 0;
+	for (c in clientes)
+		if (c.cpf == cpf)
+			return c;
+}
+
+cliente findClienteOnArray(char[100] nome, cliente[] clientes)
+{
+	int index1, index2;
+	bool isEqual = true;
+	index1 = 0;
+	for (c in clientes)
+	{
+		index2 = 0;
+		for (s in nome)
+		{
+			if (c.nome[index2] != s)
+				isEqual = false;
+			index2++;
+		}
+		if (isEqual)
+			return c;
+		index++;
+	}
+	return null;
 }
 
 bool hasClienteOnArray()
@@ -66,6 +90,20 @@ compra createCompra(){
 clientescompras createClienteCompraLink(){
 
 }
+
+data getData(clientescompras clicom){
+	
+}
+
+cliente getCliente(clientescompras clicom){
+
+}
+
+compra getCompra(clientescompras clicom){
+
+}
+
+clientescompras getClienteCompra
 // Inicialização do programa
 void fillDatabase()
 {
